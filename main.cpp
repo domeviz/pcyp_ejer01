bb138875c14b0ee640e60781a419827ea5954cbb
Domenica Vizcarra <domeviz3410@gmail.com>
2023-11-23 23:52:58 -0500

    Deber 1: Implementar la suma de dos vectores utilizando operaciones vectoriales (AVX).

--- main.cpp
+++ main.cpp
@@ -1,22 +1,50 @@
 #include <iostream>
 #include <immintrin.h>
+#include <cstdlib>
+#include <ctime>
 
 int main() {
 
-    __m256 a= _mm256_set_ps(1,2,3,4,5,6,7,8);
-    __m256 b=_mm256_set_ps(1,2,3,4,5,6,7,8);;
+    std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
-    /**
-     * c =[1,2,3,4,5,6,7,8]+[1,2,3,4,5,6,7,8]
-     */
+    const int tam = 1000000;
 
-    __m256 c= _mm256_add_ps(a,b);
+    float* vector_a = new float[tam];
+    float* vector_b = new float[tam];
+    float* vector_c = new float[tam];
 
-    float res[8];
-    _mm256_storeu_ps(res,c);
-
-    std::printf("Resultado:\n");
-    for (int i = 0; i < 8; ++i) {
-        std::printf("d[%d]=%.0f\n",i,res[i]);
+    for (int i = 0; i < tam; ++i) {
+        vector_a[i] = static_cast<float>(std::rand());
+        vector_b[i] = static_cast<float>(std::rand());
     }
+
+    const int vector_size = 8;
+    int i = 0;
+    for (; i < tam - vector_size + 1; i += vector_size) {
+        __m256 a = _mm256_loadu_ps(&vector_a[i]);
+        __m256 b = _mm256_loadu_ps(&vector_b[i]);
+
+        __m256 c = _mm256_add_ps(a, b);
+
+        _mm256_storeu_ps(&vector_c[i], c);
+    }
+
+    for (; i < tam; ++i) {
+        vector_c[i] = vector_a[i] + vector_b[i];
+    }
+
+    std::printf("10 primeros resultados:\n");
+    for (int i = 0; i < 10; ++i) {
+        std::printf("vector_a[%d]=%.0f", i, vector_a[i]);
+        std::printf(" + ");
+        std::printf("vector_b[%d]=%.0f", i, vector_b[i]);
+        std::printf(" = ");
+        std::printf("vector_c[%d]=%.0f\n", i, vector_c[i]);
+    }
+
+    delete[] vector_a;
+    delete[] vector_b;
+    delete[] vector_c;
+
+    return 0;
 }
